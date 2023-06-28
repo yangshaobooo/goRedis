44e2942c3d994043163d41456e2a9f5cf2b92e50
yangshaobooo <1783494374@qq.com>
2023-06-28 18:50:16 +0800

    commonReply

--- redis/protocol/reply.go
+++ redis/protocol/reply.go
@@ -1,6 +1,160 @@
 package protocol
 
+import (
+	"bytes"
+	"goRedis/interface/redis"
+	"strconv"
+)
+
+var (
+	nullBulkReplyBytes = []byte("$-1")
+	// CRLF is the line separator of redis serialization protocol
+	CRLF = "\r\n"
+)
+
+// ----- 1、Bulk Reply 字符串回复-----
+
+// BulkReply stores a binary-safe string
+type BulkReply struct {
+	Arg []byte
+}
+
+func (b *BulkReply) ToBytes() []byte {
+	if len(b.Arg) == 0 {
+		return nullBulkBytes
+	}
+	return []byte("$" + strconv.Itoa(len(b.Arg)) + CRLF + string(b.Arg) + CRLF)
+}
+func MakeBulkReply(arg []byte) *BulkReply {
+	return &BulkReply{
+		Arg: arg,
+	}
+}
+
+// -----2、 Multi Bulk Reply 数组回复-----
+
+// MultiBulkReply stores a list of string
+type MultiBulkReply struct {
+	Args [][]byte
+}
+
+func MakeMultiBulkReply(args [][]byte) *MultiBulkReply {
+	return &MultiBulkReply{
+		Args: args,
+	}
+}
+func (m *MultiBulkReply) ToBytes() []byte {
+	argLen := len(m.Args)
+	var buf bytes.Buffer
+	buf.WriteString("*" + strconv.Itoa(argLen) + CRLF)
+	for _, arg := range m.Args {
+		if arg == nil {
+			buf.WriteString(string(nullBulkBytes) + CRLF)
+		} else {
+			buf.WriteString("$" + strconv.Itoa(len(arg)) + CRLF + string(arg) + CRLF)
+		}
+	}
+	return buf.Bytes()
+}
+
+//---- 3、Status Reply 状态回复----
+
+// StatusReply stores a simple status string
+type StatusReply struct {
+	Status string
+}
+
+// MakeStatusReply creates StatusReply
+func MakeStatusReply(status string) *StatusReply {
+	return &StatusReply{
+		Status: status,
+	}
+}
+
+// ToBytes marshal redis.Reply
+func (r *StatusReply) ToBytes() []byte {
+	return []byte("+" + r.Status + CRLF)
+}
+
+// IsOKReply returns true if the given protocol is +OK
+func IsOKReply(reply redis.Reply) bool {
+	return string(reply.ToBytes()) == "+OK\r\n"
+}
+
+// -----4、 Int Reply 回复数字-----
+
+// IntReply stores an int64 number
+type IntReply struct {
+	Code int64
+}
+
+// MakeIntReply creates int protocol
+func MakeIntReply(code int64) *IntReply {
+	return &IntReply{
+		Code: code,
+	}
+}
+
+// ToBytes marshal redis.Reply
+func (r *IntReply) ToBytes() []byte {
+	return []byte(":" + strconv.FormatInt(r.Code, 10) + CRLF)
+}
+
+// ----- 5、err reply 错误回复-----
+
+// ErrorReply is an error and redis.Reply
 type ErrorReply interface {
 	Error() string
 	ToBytes() []byte
+}
+
+// StandardErrReply represents server error
+type StandardErrReply struct {
+	Status string
+}
+
+// MakeErrReply creates StandardErrReply
+func MakeErrReply(status string) *StandardErrReply {
+	return &StandardErrReply{
+		Status: status,
+	}
+}
+
+// ToBytes marshal redis.Reply
+func (r *StandardErrReply) ToBytes() []byte {
+	return []byte("-" + r.Status + CRLF)
+}
+
+func (r *StandardErrReply) Error() string {
+	return r.Status
+}
+
+// IsErrorReply returns true if the given protocol is error
+func IsErrorReply(reply redis.Reply) bool {
+	return reply.ToBytes()[0] == '-'
+}
+
+/* ---- 6、Multi Raw Reply ---- */
+
+// MultiRawReply store complex list structure, for example GeoPos command
+type MultiRawReply struct {
+	Replies []redis.Reply
+}
+
+// MakeMultiRawReply creates MultiRawReply
+func MakeMultiRawReply(replies []redis.Reply) *MultiRawReply {
+	return &MultiRawReply{
+		Replies: replies,
+	}
+}
+
+// ToBytes marshal redis.Reply
+func (r *MultiRawReply) ToBytes() []byte {
+	argLen := len(r.Replies)
+	var buf bytes.Buffer
+	buf.WriteString("*" + strconv.Itoa(argLen) + CRLF)
+	for _, arg := range r.Replies {
+		buf.Write(arg.ToBytes())
+	}
+	return buf.Bytes()
 }
