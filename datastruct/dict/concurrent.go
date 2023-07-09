1945a1b299d2a9c69d02792212fafc95badad98a
yangshaobooo <1783494374@qq.com>
2023-07-09 19:22:39 +0800

    dict2

--- datastruct/dict/concurrent.go
+++ datastruct/dict/concurrent.go
@@ -3,6 +3,7 @@ package dict
 import (
 	"math"
 	"sync"
+	"sync/atomic"
 )
 
 // ConcurrentDict is thread safe map using sharding lock
@@ -35,7 +36,299 @@ func computeCapacity(param int) (size int) {
 }
 
 // MakeConcurrent creates ConcurrentDict with the given shard count
-//func MakeConcurrent(shardCount int) *ConcurrentDict {
-//	shardCount = computeCapacity(shardCount)
-//	table := make([]*shard, shardCount)
+func MakeConcurrent(shardCount int) *ConcurrentDict {
+	shardCount = computeCapacity(shardCount) // 计算需要多少个map
+	table := make([]*shard, shardCount)
+	for i := 0; i < shardCount; i++ {
+		table[i] = &shard{
+			m: make(map[string]interface{}), // 初始化每一个map
+		}
+	}
+	d := &ConcurrentDict{
+		count:      0,
+		table:      table,
+		shardCount: shardCount,
+	}
+	return d
+}
+
+const prime32 = uint32(16777619) // 设定好的一个哈希质数，可以减少哈希冲突
+
+// 哈希算法: FNV-1a哈希算法
+func fnv32(key string) uint32 {
+	hash := uint32(2166136261)
+	for i := 0; i < len(key); i++ {
+		hash *= prime32
+		hash ^= uint32(key[i])
+	}
+	return hash
+}
+
+// 计算哈希值对应在哪个位置
+func (dict *ConcurrentDict) spread(hashCode uint32) uint32 {
+	if dict == nil {
+		panic("dict is nil")
+	}
+	tableSize := uint32(len(dict.table))
+	// 减1的目的是为了不超出索引范围
+	return (tableSize - 1) & hashCode // 长度和哈希值按位与，和go的map用的一样。
+}
+
+// getShard 得到指定的map片
+func (dict *ConcurrentDict) getShard(index uint32) *shard {
+	if dict == nil {
+		panic("dict is nil")
+	}
+	return dict.table[index]
+}
+
+// Get returns the binding value and whether the key is exist
+func (dict *ConcurrentDict) Get(key string) (val interface{}, exists bool) {
+	if dict == nil {
+		panic("dict is nil")
+	}
+	hashCode := fnv32(key)         // 计算哈希值
+	index := dict.spread(hashCode) // 计算哈希值对应的map
+	s := dict.getShard(index)      // 得到对应的map
+	s.mutex.RLock()                // 上读锁  源码里用的写锁，博客里用的读锁
+	defer s.mutex.RUnlock()
+	val, exists = s.m[key]
+	return
+}
+
+// GetWithLock get without lock
+func (dict *ConcurrentDict) GetWithLock(key string) (val interface{}, exists bool) {
+	if dict == nil {
+		panic("dict is nil")
+	}
+	hashCode := fnv32(key)
+	index := dict.spread(hashCode)
+	s := dict.getShard(index)
+	val, exists = s.m[key]
+	return
+}
+
+// Len returns the number of dict
+func (dict *ConcurrentDict) Len() int {
+	if dict == nil {
+		panic("dict is nil")
+	}
+	// atomic.LoadInt32() 使用原子操作，保证在并发环境中取得正确的count
+	return int(atomic.LoadInt32(&dict.count))
+}
+
+// Put puts key value into dict and returns the number of new inserted key-value
+func (dict *ConcurrentDict) Put(key string, val interface{}) (result int) {
+	if dict == nil {
+		panic("dict is nil")
+	}
+	hashCode := fnv32(key)         // 计算哈希值
+	index := dict.spread(hashCode) // 计算对应的map
+	s := dict.getShard(index)      // 得到对应的map
+	s.mutex.Lock()                 // 写入要用写锁
+	defer s.mutex.Unlock()
+
+	if _, ok := s.m[key]; ok {
+		s.m[key] = val // 如果已经存在
+		return 0
+	}
+	dict.addCount() // 字典中key-value 数量+1
+	s.m[key] = val  //
+	return 1
+}
+
+func (dict *ConcurrentDict) PutWithLock(key string, val interface{}) (result int) {
+	if dict == nil {
+		panic("dict is nil")
+	}
+	hashCode := fnv32(key)
+	index := dict.spread(hashCode)
+	s := dict.getShard(index)
+	if _, ok := s.m[key]; ok {
+		s.m[key] = val
+		return 0
+	}
+	dict.addCount()
+	s.m[key] = val
+	return 1
+}
+
+// PutIfAbsent puts value if the key is not exists and returns the number of updated key-value
+func (dict *ConcurrentDict) PutIfAbsent(key string, val interface{}) (result int) {
+	if dict == nil {
+		panic("dict is nil")
+	}
+	hashCode := fnv32(key)
+	index := dict.spread(hashCode)
+	s := dict.getShard(index)
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if _, ok := s.m[key]; ok {
+		return 0
+	}
+	s.m[key] = val
+	dict.addCount()
+	return 1
+}
+
+func (dict *ConcurrentDict) PutIfAbsentWithLock(key string, val interface{}) (result int) {
+	if dict == nil {
+		panic("dict is nil")
+	}
+	hashCode := fnv32(key)
+	index := dict.spread(hashCode)
+	s := dict.getShard(index)
+
+	if _, ok := s.m[key]; ok {
+		return 0
+	}
+	s.m[key] = val
+	dict.addCount()
+	return 1
+}
+
+// PutIfExists puts value if the key is exist and returns the number of inserted key-value
+func (dict *ConcurrentDict) PutIfExists(key string, val interface{}) (result int) {
+	if dict == nil {
+		panic("dict is nil")
+	}
+	hashCode := fnv32(key)
+	index := dict.spread(hashCode)
+	s := dict.getShard(index)
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if _, ok := s.m[key]; ok {
+		s.m[key] = val
+		return 1
+	}
+	return 0
+}
+
+func (dict *ConcurrentDict) PutIfExistsWithLock(key string, val interface{}) (result int) {
+	if dict == nil {
+		panic("dict is nil")
+	}
+	hashCode := fnv32(key)
+	index := dict.spread(hashCode)
+	s := dict.getShard(index)
+
+	if _, ok := s.m[key]; ok {
+		s.m[key] = val
+		return 1
+	}
+	return 0
+}
+
+// Remove removes the key and return the number of deleted key-value
+func (dict *ConcurrentDict) Remove(key string) (result int) {
+	if dict == nil {
+		panic("dict is nil")
+	}
+	hashCode := fnv32(key)
+	index := dict.spread(hashCode)
+	s := dict.getShard(index)
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if _, ok := s.m[key]; ok {
+		delete(s.m, key)
+		dict.decreaseCount()
+		return 1
+	}
+	return 0
+}
+
+func (dict *ConcurrentDict) RemoveWithLock(key string) (val interface{}, result int) {
+	if dict == nil {
+		panic("dict is nil")
+	}
+	hashCode := fnv32(key)
+	index := dict.spread(hashCode)
+	s := dict.getShard(index)
+
+	if val, ok := s.m[key]; ok {
+		delete(s.m, key)
+		dict.decreaseCount()
+		return val, 1
+	}
+	return val, 0
+}
+
+func (dict *ConcurrentDict) addCount() int32 {
+	return atomic.AddInt32(&dict.count, 1)
+}
+
+func (dict *ConcurrentDict) decreaseCount() int32 {
+	return atomic.AddInt32(&dict.count, -1)
+}
+
+// ForEach traversal the dict
+// it may not visits new entry inserted during traversal
+func (dict *ConcurrentDict) ForEach(consumer Consumer) {
+	if dict == nil {
+		panic("dict is nil")
+	}
+
+	for _, s := range dict.table {
+		s.mutex.RLock()
+		f := func() bool {
+			defer s.mutex.RUnlock()
+			for key, value := range s.m {
+				continues := consumer(key, value)
+				if !continues {
+					return false
+				}
+			}
+			return false
+		}
+		if !f() {
+			break
+		}
+	}
+}
+
+func (dict *ConcurrentDict) keys() []string {
+	keys := make([]string, dict.Len())
+	i := 0
+	dict.ForEach(func(key string, val interface{}) bool {
+		if i < len(keys) {
+			keys[i] = key
+			i++
+		} else {
+			keys = append(keys, key)
+		}
+		return true
+	})
+	return keys
+}
+
+// RandomKey returns a key randomly
+func (shard *shard) RandomKey() string {
+	if shard == nil {
+		panic("shard is nil")
+	}
+	shard.mutex.RLock()
+	defer shard.mutex.RUnlock()
+
+	for key := range shard.m {
+		return key
+	}
+	return ""
+}
+
+// RandomKeys randomly returns keys of the given number, may contain duplicated key
+//func (dict *ConcurrentDict) RandomKeys(limit int) []string {
+//
 //}
+
+func (dict *ConcurrentDict) RandomDistinctKeys(limit int) []string {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (dict *ConcurrentDict) Clear() {
+	//TODO implement me
+	panic("implement me")
+}
